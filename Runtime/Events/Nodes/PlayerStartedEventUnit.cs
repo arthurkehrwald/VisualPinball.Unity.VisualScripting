8a4f6a31801d60ee2600dc4cf24d0892eb8fe002
Jason Millard <jsm174@gmail.com>
2021-12-23 10:09:06 -0500

    events: Added OnPlayerStarted event

---
+++ Runtime/Events/Nodes/PlayerStartedEventUnit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Unity.VisualScripting;
+
+namespace VisualPinball.Unity.VisualScripting
+{
+    [UnitTitle("On Player Started Event")]
+    [UnitCategory("Events\\Visual Pinball")]
+    public sealed class PlayerStartedEventUnit : EventUnit<EventArgs>
+    { 
+        protected override bool register => true;
+
+        public override EventHook GetHook(GraphReference reference)
+        {
+            return new EventHook(EventNames.PlayerStartedEvent);
+        }
+
+        protected override void Definition()
+        {
+            base.Definition();
+        }
+    }
+}
