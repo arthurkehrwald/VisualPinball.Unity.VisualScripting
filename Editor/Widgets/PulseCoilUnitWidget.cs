00ca19a53b74852258aa705a7637324a2715abb4
freezy <freezy@kodi.tv>
2022-01-25 00:19:07 +0100

    Add coil pulse node.

---
+++ Editor/Widgets/PulseCoilUnitWidget.cs
@@ -0,0 +1,56 @@
+﻿// Visual Pinball Engine
+// Copyright (C) 2022 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+// ReSharper disable UnusedType.Global
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+
+namespace VisualPinball.Unity.VisualScripting.Editor
+{
+	[Widget(typeof(PulseCoilUnit))]
+	public sealed class PulseCoilUnitWidget : GleUnitWidget<PulseCoilUnit>
+	{
+		private VariableNameInspector _coilIdInspector;
+		private readonly Func<Metadata, VariableNameInspector> _setCoilInspectorConstructor;
+
+		public PulseCoilUnitWidget(FlowCanvas canvas, PulseCoilUnit unit) : base(canvas, unit)
+		{
+			_setCoilInspectorConstructor = meta => new VariableNameInspector(meta, GetNameSuggestions);
+		}
+
+		public override Inspector GetPortInspector(IUnitPort port, Metadata meta)
+		{
+			if (port == unit.Id) {
+				InspectorProvider.instance.Renew(ref _coilIdInspector, meta, _setCoilInspectorConstructor);
+				return _coilIdInspector;
+			}
+
+			return base.GetPortInspector(port, meta);
+		}
+
+		private IEnumerable<string> GetNameSuggestions()
+		{
+			if (!GameObjectAvailable) {
+				return new List<string>();
+			}
+			var gle = Gle;
+			return gle == null ? new List<string>() : gle.AvailableCoils.Select(coil => coil.Id).ToList();
+		}
+	}
+}
---
+++ Runtime/Nodes/Coils/PulseCoilUnit.cs
@@ -0,0 +1,76 @@
+﻿// Visual Pinball Engine
+// Copyright (C) 2022 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace VisualPinball.Unity.VisualScripting
+{
+	[UnitTitle("Pulse Coil")]
+	[UnitSurtitle("Gamelogic Engine")]
+	[UnitCategory("Visual Pinball")]
+	public class PulseCoilUnit : GleUnit
+	{
+		[DoNotSerialize]
+		[PortLabelHidden]
+		public ControlInput InputTrigger;
+
+		[DoNotSerialize]
+		[PortLabelHidden]
+		public ControlOutput OutputTrigger;
+
+		[DoNotSerialize]
+		[PortLabel("Coil ID")]
+		public ValueInput Id { get; private set; }
+
+		[DoNotSerialize]
+		[PortLabel("Duration (ms)")]
+		public ValueInput PulseDuration { get; private set; }
+
+		protected override void Definition()
+		{
+			InputTrigger = ControlInput(nameof(InputTrigger), Process);
+			OutputTrigger = ControlOutput(nameof(OutputTrigger));
+
+			Id = ValueInput<string>(nameof(Id), string.Empty);
+			PulseDuration = ValueInput<int>(nameof(PulseDuration), 80);
+
+			Requirement(Id, InputTrigger);
+			Succession(InputTrigger, OutputTrigger);
+		}
+
+		private ControlOutput Process(Flow flow)
+		{
+			if (!AssertGle(flow)) {
+				Debug.LogError("Cannot find GLE.");
+				return OutputTrigger;
+			}
+
+			if (!AssertPlayer(flow)) {
+				Debug.LogError("Cannot find GLE.");
+				return OutputTrigger;
+			}
+
+			var id = flow.GetValue<string>(Id);
+			var pulseDuration = flow.GetValue<int>(PulseDuration);
+
+			Gle.SetCoil(id, true);
+			Player.ScheduleAction(pulseDuration, () => Gle.SetCoil(id, false));
+
+			return OutputTrigger;
+		}
+	}
+}
